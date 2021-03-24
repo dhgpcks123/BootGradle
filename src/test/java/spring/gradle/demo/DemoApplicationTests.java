65b1cea517c8878161d49fb5f99de563e2ffd0dc
hyechan <dhgpcks@gmail.com>
2021-03-24 17:14:49 +0900

    git init

---
+++ src/test/java/spring/gradle/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.gradle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
